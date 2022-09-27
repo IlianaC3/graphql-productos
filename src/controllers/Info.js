1dfa745ae0cb4e95fc1e3d213c200ff912f50e25
IlianaC3 <103341704+IlianaC3@users.noreply.github.com>
2022-09-27 19:22:44 -0300

    Add files via upload

---
+++ src/controllers/Info.js
@@ -0,0 +1,36 @@
+const { randomCalc } = require('../operations/randoms/randomCal')
+const InfoClass = require('../services/Info');
+const InfoData = new InfoClass();
+const forkFunction = require('../operations/randoms/Fork')
+
+class GeneralInfo {
+    async Info(req, res, next) {
+        let data = await InfoData.DataProject().then((result) => { return result});
+        let test = await InfoData.ForLoop().then((result) => { return result})
+        res.status(200).json({
+            message: 'Datos',
+            elementos: data,
+            test: test
+        });
+    };
+    
+    //con child process
+    async RandomsChildProccess(req, res, next) {
+        let random = req.query.cant === undefined ? 1e9 : req.query.cant;
+        const forkResult = await forkFunction(random).then( result => { return result });
+        res.send(forkResult)
+    };
+    
+    //sin child process
+    async Randoms (req, res, next) {
+        let random = req.query.cant === undefined ? 1e9 : req.query.cant;
+        let result = randomCalc(random)
+        if (result > 0) {
+            res.send('La sumatoria total fue '+ result)
+        }
+    };
+}
+
+
+
+module.exports = GeneralInfo;
